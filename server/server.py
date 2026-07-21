e864f3b34f1ea4575b449f6e90ce3f312caac251
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-21 22:10:49 +0530

    server boiler code added

---
+++ server/server.py
@@ -0,0 +1,19 @@
+from dependancyService.dependancyService import DependancyService
+
+class Server:
+    
+    dependancy = DependancyService()
+    
+    def __init__(self):
+        pass
+    
+    def send_model(self):
+        
+        pass
+    
+    def receive_weights():
+        pass
+    
+    def aggregate(self):
+        pass
+    
