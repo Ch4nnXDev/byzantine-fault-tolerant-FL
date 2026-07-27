0a1398667fe4c4b4ff8ba5feadb46aba404ac7af
Channa2146@gmail.com <Channa2146@gmail.com>
2026-07-27 09:48:02 +0530

    main.py implemented run()

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
 from clients.client import Client
+from server.server import Server
 
 class Main:
     def __init__(self):
-        pass
-    
+        self.server = Server()
+        self.clients = []
+        
+        
     def create_clients(self, num):
-        clients = []
         for i in range(num):
-           client = Client()
-           client.add(client)
-        return clients
-    
-    
+            client = Client()
+            self.clients.append(client)
+        
             
+    def run(self, rounds):
+        loss_collect = []
+        for round in range(rounds):
+            
+            print(f"Epoch {round + 1}")
+            
+  
+            
+            self.server.send_model(self.clients)
+            
+            for client in self.clients:
+                gg = client.train_model()
+                print(gg)
+                
+                
+                
+            for clients in self.clients:
+                
+                self.server.receive_weights(
+                    clients.get_weights()
+                )
+                
+            self.server.aggregate()
+            
+            accuracy, loss = self.server.evaluate()
+
+
+            print(
+                f"Global Accuracy: {accuracy:.2%}"
+            )
+
+            print(
+                f"Global Loss: {loss:.4f}"
+            )
+            
+    
+
+if __name__ == "__main__":
+
+    app = Main()
+
+    app.create_clients(5)
+
+    app.run(5)
+    
+                
+            
+        
+        
+   
         
         
         
